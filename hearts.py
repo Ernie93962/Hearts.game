84d4af7fa7ec5659a4c60c51f849a8659bddc9fa
Ernest A. May <962eam19@stu.olatheschools.org>
2021-01-11 20:42:36 -0600

    suite rules

--- hearts.py
+++ hearts.py
@@ -12,7 +12,6 @@ SUITE_CLUBS = 1
 SUITE_HEARTS = 2
 SUITE_DIAMONDS = 3
 
-name = input('Please enter your name: ')
 deck = []
 player0=[]
 player1=[]
@@ -26,7 +25,7 @@ trickPile2 = []
 trickPile3 = []
 trickPiles = [trickPile0,trickPile1,trickPile2,trickPile3]
 scores = []
-playerNames = ['Jim', 'Doug', 'Bob', name]
+playerNames = ['Jim', 'Doug', 'Bob']
 human = 3
 
 def printCards(cards):
@@ -102,15 +101,21 @@ def humanPlayCard(player,isFirstcard=False):
     printCards(player)
     print("Current Trick is:")
     printCurrentHand(currentHand) 
+    validCards = []
+    if len(currentHand) > 0:
+        leaderSuite = getSuite(currentHand[0][1])
+        validCards = getCardsBySuite(player,leaderSuite)
     while True:  
         humanCard = input('Play card: ')
         humanCard = convertHumanCard(humanCard)
         if isFirstcard:
             if humanCard == 14:
                 break
-        else:
-            if humanCard in player:
+        elif len(validCards) > 0:
+            if humanCard in validCards:
                 break
+        else:
+            break
     player.remove(humanCard)
     return(humanCard)
 
@@ -134,7 +139,7 @@ def playCard(player,isFirstCard=False):
 
 def convertHumanCard(card):
     suite = card[0].upper()
-    value = card[1:]
+    value = card[1:].upper()
     cardNumber = 0
     if( suite == 'C'):
         cardNumber = 13
@@ -213,39 +218,45 @@ def shuffleDeck(amount=1):
     for i in range(0,amount):
         shuffle()
 
-random.seed()
+def main():
+    random.seed()
+    name = input('Please enter your name: ')
+    playerNames.append(name)
 
-initalizeDeck()
-shuffleDeck(1000)
-deal()
-aPlayer = getFirstPlayer()
-debugString = ''
-for card in players[aPlayer]:
-    txt = getCardString(card)
-    debugString += txt+' '
-#print(debugString)
-playedCard = None
-isFirstPlayer = True
-for round in range(0,13):
-    for trick in range(0,4):
-        if aPlayer == human:
-            playedCard = humanPlayCard(players[aPlayer],isFirstPlayer)
-        else:
-            playedCard = playCard(players[aPlayer],isFirstPlayer)
-        currentHand.append([aPlayer,playedCard])
-        aPlayer=(aPlayer+1)%4
-        isFirstPlayer = False
-    trickWinner = findTrickWinner(currentHand)
-    print("Then trick winner is: " + playerNames[trickWinner[0]])
-    takeTrick(trickWinner[0],currentHand)
-    currentHand.clear()
-    aPlayer = trickWinner[0]
-findScore()
-winner = 0
-winnerScore = 26
-for index,score in enumerate(scores):
-    print(playerNames[index]+': '+str(scores[index]))
-    if scores[index] < winnerScore:
-        winnerScore = scores[index]
-        winner = index
-print('The winner is: ' + playerNames[winner])
+    initalizeDeck()
+    shuffleDeck(1000)
+    deal()
+    aPlayer = getFirstPlayer()
+    debugString = ''
+    for card in players[aPlayer]:
+        txt = getCardString(card)
+        debugString += txt+' '
+    #print(debugString)
+    playedCard = None
+    isFirstPlayer = True
+    for round in range(0,13):
+        for trick in range(0,4):
+            if aPlayer == human:
+                playedCard = humanPlayCard(players[aPlayer],isFirstPlayer)
+            else:
+                playedCard = playCard(players[aPlayer],isFirstPlayer)
+            currentHand.append([aPlayer,playedCard])
+            aPlayer=(aPlayer+1)%4
+            isFirstPlayer = False
+        trickWinner = findTrickWinner(currentHand)
+        print("Then trick winner is: " + playerNames[trickWinner[0]] + ' [' + playerNames[currentHand[0][0]] + ':' + getCardString(currentHand[0][1]) + ', ' + playerNames[currentHand[1][0]] + ':' + getCardString(currentHand[1][1]) + ', ' + playerNames[currentHand[2][0]] + ':' + getCardString(currentHand[2][1]) + ', ' + playerNames[currentHand[3][0]] + ':' + getCardString(currentHand[3][1]) + ']')
+        takeTrick(trickWinner[0],currentHand)
+        currentHand.clear()
+        aPlayer = trickWinner[0]
+    findScore()
+    winner = 0
+    winnerScore = 26
+    for index,score in enumerate(scores):
+        print(playerNames[index]+': '+str(scores[index]))
+        if scores[index] < winnerScore:
+            winnerScore = scores[index]
+            winner = index
+    print('The winner is: ' + playerNames[winner])
+
+if __name__ == "__main__":
+    main()
